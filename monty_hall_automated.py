f3b3a0a2ed058b9ac93a229539e2e8262083f12f
enesdilsiz <54660460+enesdilsiz@users.noreply.github.com>
2020-10-03 18:03:30 +0300

    Add files via upload

--- monty_hall_automated.py
+++ monty_hall_automated.py
@@ -1,10 +1,6 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sun Sep 27 12:57:46 2020
-
-@author: enesd
+Monty Hall Problem Statistics - @author: enesdilsiz
 """
-
 import random
 
 print(
@@ -21,21 +17,9 @@ print(
 )
 
 print("""
-Yarışmamıza hoş geldiniz. 0, 1 ve 2 numaralı 3 tane kapımız bulunmaktadır.
-Kapıların 2 tanesinin arkasında keçi, 1 tanesinin arkasında araba bulunmaktadır.
-Öncelikle bu 3 kapı arasından birini seçmeniz gerekmektedir. Daha seçtiğiniz
-kapı haricindeki 2 kapıdan arkasında keçi olan kapı açılıp kararınızı 
-değiştirmek isteyip istemediğiniz sorulacaktır. Seçiminize göre en son
-seçtiğiniz kapının arkasındaki ödül sizin olacaktır. Başarılar dilerim. :)
-      """)
-
-#X = ["goat","goat","car"]
-#random.shuffle(X)
-
-#inp = input("Please enter the one you want to choose from doors 0, 1 and 2:  ")
-#inp= int(inp) 
-
-#inp = random.randint(0,2)
+      This program tests the Monty Hall Problem with number of iterations 
+      and calculates the likelihood in different situations.
+""")
 
 changed_win=0
 changed_not_win=0
@@ -49,34 +33,27 @@ for j in range(1000):
     
     for i in range(3):
         if X[i] == "goat" and i != inp:
-#            print("\n" + str(i) + " has a goat behind it.")
-#            yesno = input("\nDo you want to change your door?(yes/no)  ")
             yesno = random.choice(['yes', 'no']) 
             a=i
             
             if yesno == "yes":
                 new = 3-inp-a
                 if X[new] == "goat":
-#                    print("\nYou changed your door to " + str(new) + " and win a goat")
                     changed_not_win += 1
                     break
                 elif X[new] == "car":
-#                    print("\nYou changed your door to " + str(new) + " and win a car")
                     changed_win += 1
                     break
             elif yesno == "no":
                 if X[inp] == "goat":
-#                    print("\nYou keep your door and win a goat")
                     not_changed_not_win += 1
                     break
                 elif X[inp] == "car":
-#                    print("\nYou keep your door and win a car")
                     not_changed_win += 1
                     break
-#print("\nThe gates were like: ")
-#print(X)
+
 changed_win_ratio = changed_win/(changed_win + changed_not_win)
 not_changed_win_ratio = not_changed_win/(not_changed_win + not_changed_not_win)
 
-print("Karar değiştirildiğindeki başarı oranı: ", changed_win_ratio)
-print("Karar değiştirilmediğindeki başarı oranı: ", not_changed_win_ratio)
+print("Success rate when the decision is changed: ", changed_win_ratio)
+print("Success rate when the decision is NOT changed: ", not_changed_win_ratio)
--- monty_hall_problem.py
+++ monty_hall_problem.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
 """
-Created on Fri Sep 25 16:04:50 2020
-
-@author: enesd
+Monty Hall Problem Game - @author: enesdilsiz
 """
 import random
 
-print(
-"""
+print("""
   __  __             _           _    _       _ _   _____           _     _                
  |  \/  |           | |         | |  | |     | | | |  __ \         | |   | |               
  | \  / | ___  _ __ | |_ _   _  | |__| | __ _| | | | |__) | __ ___ | |__ | | ___ _ __ ___  
@@ -16,45 +12,47 @@ print(
  |_|  |_|\___/|_| |_|\__|\__, | |_|  |_|\__,_|_|_| |_|   |_|  \___/|_.__/|_|\___|_| |_| |_|
                           __/ |                                                            
                          |___/                                                             
-"""
-)
+""")
 
 print("""
-Yarışmamıza hoş geldiniz. 0, 1 ve 2 numaralı 3 tane kapımız bulunmaktadır.
-Kapıların 2 tanesinin arkasında keçi, 1 tanesinin arkasında araba bulunmaktadır.
-Öncelikle bu 3 kapı arasından birini seçmeniz gerekmektedir. Daha seçtiğiniz
-kapı haricindeki 2 kapıdan arkasında keçi olan kapı açılıp kararınızı 
-değiştirmek isteyip istemediğiniz sorulacaktır. Seçiminize göre en son
-seçtiğiniz kapının arkasındaki ödül sizin olacaktır. Başarılar dilerim. :)
-      """)
+      Welcome to the contest. We have 3 doors numbered 0, 1 and 2. 
+      There are goats behind 2 of the doors and a car behind 1 of them.
+      First of all, you have to choose one of these 3 doors.
+      You will be asked if you want to change your decision 
+      by opening the door with a goat behind it from 2 doors 
+      other than the one you have selected. The prize behind 
+      the last door you choose will be yours. Good luck. :)
+""")
 
 X = ["goat","goat","car"]
+
 random.shuffle(X)
 
 inp = input("Please enter the one you want to choose from doors 0, 1 and 2:  ")
+
 inp= int(inp) 
 
 
 for i in range(3):
     if X[i] == "goat" and i != inp:
-        print("\n" + str(i) + " has a goat behind it.")
-        yesno = input("\nDo you want to change your door?(yes/no)  ")
+        print("\nDoor " + str(i) + " has a goat behind it.")
+        yesno = input("\nWould you like to change your door with the other door?(yes/no)  ")
         a=i
         
         if yesno == "yes":
             new = 3-inp-a
             if X[new] == "goat":
-                print("\nYou changed your door to " + str(new) + " and win a goat")
+                print("\nYou change your door from " + str(inp) + " to " + str(new) + " and win a GOAT.")
                 break
             elif X[new] == "car":
-                print("\nYou changed your door to " + str(new) + " and win a car")
+                print("\nYou change your door from " + str(inp) + " to " + str(new) + " and win a CAR!")
                 break
         elif yesno == "no":
             if X[inp] == "goat":
-                print("\nYou keep your door and win a goat")
+                print("\nYou keep your door and win a GOAT.")
                 break
             elif X[inp] == "car":
-                print("\nYou keep your door and win a car")
+                print("\nYou keep your door and win a CAR!")
                 break
-print("\nThe gates were like: ")
-print(X)
+
+print("\nThe gates were like 0: %s, 1: %s, 2: %s " %(X[0],X[1],X[2]))
